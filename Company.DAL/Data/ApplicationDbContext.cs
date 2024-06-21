200c6e3c1984a2389ece757c51ea859e2a2a2521
SallamChalbi <sallamchalbi@gmail.com>
2024-06-21 20:17:40 +0100

    DAL for Department Module

---
+++ Company.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Company.DAL.Data.Configurations;
+using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        //{
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("server = .; Database = MVCProject; trusted_Connection = true");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        public DbSet<Department> Departments { get; set; }
+    }
+}
